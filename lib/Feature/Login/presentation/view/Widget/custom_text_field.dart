c57ff6a0fd9c81e0d408a63c4f96a84148f89d1b
MoazMohamedHamzaGaber <moaz68860@gmail.com>
2024-02-21 03:45:22 +0200

    Custom text field

--- lib/Feature/Login/presentation/view/Widget/custom_text_field.dart
+++ lib/Feature/Login/presentation/view/Widget/custom_text_field.dart
@@ -1,36 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:marketi/Feature/Login/presentation/manage/cubit/login_cubit.dart';
+import 'package:marketi/Feature/Login/presentation/manage/cubit/login_states.dart';
 
 import '../../../../../core/utils/components.dart';
 
 class CustomTextField extends StatelessWidget {
-  const CustomTextField({super.key, this.emailController, this.passwordController});
+   CustomTextField({super.key, this.emailController, this.passwordController});
 
   final emailController;
   final passwordController;
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        buildTextField(
-          controller: emailController,
-          textInputType: TextInputType.emailAddress,
-          prefixIcon: Icons.mail_outline,
-          text: 'Username or Email',
-          validateText: 'Email is empty',
-        ),
-        const SizedBox(
-          height: 14,
-        ),
-        buildTextField(
-          controller: passwordController,
-          obscureText: true,
-          textInputType: TextInputType.visiblePassword,
-          prefixIcon: Icons.lock_outline,
-          text: '**************',
-          validateText: 'Password is empty',
-          suffixIcon: Icons.visibility,
-        ),
-      ],
+    return BlocConsumer<LoginCubit,LoginStates>(
+      listener: (BuildContext context, state) {  },
+      builder: (BuildContext context, Object? state) {
+        var cubit=LoginCubit().get(context);
+        return Column(
+          children: [
+            buildTextField(
+              controller: emailController,
+              textInputType: TextInputType.emailAddress,
+              prefixIcon: Icons.mail_outline,
+              text: 'Username or Email',
+              validateText: 'Email is empty',
+            ),
+            const SizedBox(
+              height: 14,
+            ),
+            buildTextField(
+                controller: passwordController,
+                obscureText: cubit.obscureText,
+                textInputType: TextInputType.visiblePassword,
+                prefixIcon: Icons.lock_outline,
+                text: '**************',
+                validateText: 'Password is empty',
+                suffixIcon:cubit.obscureText? Icons.visibility :Icons.visibility_off,
+                suffixFunction: (){
+                  cubit.changeObscureText();
+                }
+            ),
+          ],
+        );
+      },
     );
   }
 }
