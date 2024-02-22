dab816b76fa9a01724cacb7a1caff28a9424f513
MoazMohamedHamzaGaber <moaz68860@gmail.com>
2024-02-22 04:46:28 +0200

    Register Cubit

--- lib/Feature/Register/feature/manage/cubit/register_cubit.dart
+++ lib/Feature/Register/feature/manage/cubit/register_cubit.dart
@@ -1,5 +1,9 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:marketi/Feature/Register/data/model/register_model.dart';
 
+import '../../../../../core/utils/components.dart';
+import '../../../../../core/utils/api_service.dart';
 import 'register_states.dart';
 
 class RegisterCubit extends Cubit<RegisterStates>
@@ -13,5 +17,37 @@ class RegisterCubit extends Cubit<RegisterStates>
   void changeObscureText(){
     obscureText=!obscureText;
     emit(RegisterChangeObscureTextStates());
+  }
+
+  RegisterModel? registerModel;
+  void userCreate({
+    required String email,
+    required String name,
+    required String password,
+    required String passwordConfirm,
+     context
+  }){
+    emit(RegisterLoadingStates());
+    buildShowLoading(context);
+    ApiService.postData(
+        url: '/api/v1/auth/signup',
+        data: {
+          'name':name,
+          'email':email,
+          'password':password,
+          'passwordConfirm':passwordConfirm,
+        }
+    ).then((value){
+      registerModel=RegisterModel.fromJson(value.data);
+      emit(RegisterSuccessStates());
+      Navigator.pop(context);
+      showToast(text: 'An account has been created');
+    }).catchError((error)
+    {
+      print(error.toString());
+      emit(RegisterErrorStates(error.toString()));
+      Navigator.pop(context);
+    }
+    );
   }
 }
