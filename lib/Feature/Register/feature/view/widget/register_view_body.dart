3ba37d8781a02db12d9d4628b8ba918338d4d558
MoazMohamedHamzaGaber <moaz68860@gmail.com>
2024-02-22 04:47:02 +0200

    Register View Body

--- lib/Feature/Register/feature/view/widget/register_view_body.dart
+++ lib/Feature/Register/feature/view/widget/register_view_body.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:marketi/Feature/Login/presentation/view/login_view.dart';
+import 'package:marketi/Feature/Register/feature/manage/cubit/register_cubit.dart';
+import 'package:marketi/Feature/Register/feature/manage/cubit/register_states.dart';
+import 'package:marketi/Feature/Register/feature/view/widget/custom_botton_register.dart';
 import 'package:marketi/Feature/Register/feature/view/widget/custom_image.dart';
 import 'package:marketi/Feature/Register/feature/view/widget/section_text_field.dart';
+import 'package:marketi/core/utils/app_assets.dart';
 import 'package:marketi/core/utils/components.dart';
+import 'package:marketi/core/utils/constant.dart';
 import 'package:marketi/core/widget/custom_or_continue_with.dart';
 
 class RegisterViewBody extends StatefulWidget {
@@ -22,36 +29,53 @@ class _RegisterViewBodyState extends State<RegisterViewBody> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Form(
-        key: formKey,
-        child: SingleChildScrollView(
-          physics: const BouncingScrollPhysics(),
-          child: Column(
-            children: [
-              const CustomImage(),
-              SectionTextField(
-                emailController: emailController,
-                passwordController: passwordController,
-                confirmPasswordController: confirmPasswordController,
-                nameController: nameController,
+      child: BlocConsumer<RegisterCubit, RegisterStates>(
+        listener: (BuildContext context, state) {
+          if(state is RegisterErrorStates)
+          {
+            buildShowDialog(
+              context: context,
+              image: AppAssets.imagesWarning,
+              name: state.error,
+              function: (){
+                Navigator.pop(context);
+              },
+            );
+          }
+          if(state is RegisterSuccessStates)
+          {
+            navigateTo(context, const LoginView());
+          }
+        },
+        builder: (BuildContext context, Object? state) {
+          var cubit = RegisterCubit().get(context);
+          return Form(
+            key: formKey,
+            child: SingleChildScrollView(
+              physics: const BouncingScrollPhysics(),
+              child: Column(
+                children: [
+                  const CustomImage(),
+                  SectionTextField(
+                    emailController: emailController,
+                    passwordController: passwordController,
+                    confirmPasswordController: confirmPasswordController,
+                    nameController: nameController,
+                  ),
+                   CustomBottomRegister(
+                    emailController: emailController,
+                    passwordController: passwordController,
+                    confirmPasswordController: confirmPasswordController,
+                    nameController: nameController,
+                     cubit: cubit,
+                     formKey: formKey,
+                  ),
+                  const CustomOrContinueWith(),
+                ],
               ),
-              Padding(
-                padding: const EdgeInsets.only(
-                    left: 15, right: 13, bottom: 12, top: 14),
-                child: buildContainer(
-                  text: 'Sign Up',
-                  function: () {
-                    if (formKey.currentState!.validate()) {
-                      print(nameController.text);
-                      print(emailController.text);
-                    }
-                  },
-                ),
-              ),
-              const CustomOrContinueWith(),
-            ],
-          ),
-        ),
+            ),
+          );
+        },
       ),
     );
   }
