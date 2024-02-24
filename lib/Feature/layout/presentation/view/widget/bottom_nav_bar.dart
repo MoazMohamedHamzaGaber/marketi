08489a5801bb99a6baa9e0d56927dc08725fc10a
MoazMohamedHamzaGaber <moaz68860@gmail.com>
2024-02-24 16:38:46 +0200

    Bottom navigation bar

---
+++ lib/Feature/layout/presentation/view/widget/bottom_nav_bar.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../manage/cubit/Layout_cubit.dart';
+import '../../manage/cubit/Layout_states.dart';
+
+class BottomNavBar extends StatefulWidget {
+   const BottomNavBar({Key? key}) : super(key: key);
+
+  @override
+  State<BottomNavBar> createState() => _BottomNavBarState();
+}
+
+class _BottomNavBarState extends State<BottomNavBar> {
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return BlocConsumer<LayoutCubit,LayoutStates>(
+      listener: (BuildContext context, state) {  },
+      builder: (BuildContext context, Object? state) {
+        var cubit=LayoutCubit().get(context);
+        return Container(
+          color: const Color(0xff2B475E),
+          height: size.width * .155,
+          child: ListView.builder(
+            itemCount: 4,
+            scrollDirection: Axis.horizontal,
+            padding: EdgeInsets.symmetric(horizontal: size.width * .024),
+            itemBuilder: (context, index) => InkWell(
+              onTap: () {
+                cubit.changeCurrentIndex(index);
+              },
+              splashColor: Colors.transparent,
+              highlightColor: Colors.transparent,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  SizedBox(height: size.width * .014),
+                  Icon(listOfIcons[index],
+                      size: size.width * .076, color: Colors.white),
+                  AnimatedContainer(
+                    duration: const Duration(milliseconds: 1500),
+                    curve: Curves.fastLinearToSlowEaseIn,
+                    margin: EdgeInsets.only(
+                      top: index ==cubit.currentIndex ? 0 : size.width * .029,
+                      right: size.width * .0422,
+                      left: size.width * .0422,
+                    ),
+                    width: size.width * .153,
+                    height: index ==cubit.currentIndex ? size.width * .014 : 0,
+                    decoration: const BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.vertical(
+                        top: Radius.circular(20),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  List<IconData> listOfIcons = [
+    Icons.home_rounded,
+    Icons.category_rounded,
+    Icons.favorite,
+    Icons.settings];
+}
