4afceffb52869894edd454111aeadb9f7f6caba3
MoazMohamedHamzaGaber <moaz68860@gmail.com>
2024-02-26 02:37:00 +0200

    Section Category

---
+++ lib/Feature/Home/presentation/view/widget/section_category.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:marketi/Feature/Home/presentation/view/widget/custom_title.dart';
+
+class SectionCategory extends StatelessWidget {
+  const SectionCategory({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        CustomTitleHome(title: 'Category', function: (){}),
+        const SizedBox(
+          height: 8,
+        ),
+      ],
+    );
+  }
+}
