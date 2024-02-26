8df6d7cdfcc2e7352e8bee65ddea55301c329184
MoazMohamedHamzaGaber <moaz68860@gmail.com>
2024-02-26 04:35:02 +0200

    Category List

---
+++ lib/Feature/Home/data/list/category_list.dart
@@ -0,0 +1,31 @@
+import '../model/category_model.dart';
+
+List<CategoryModel> categoryModel = [
+  CategoryModel(
+    name: 'Phones',
+    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBGSEKSjd3QDc-fmnm6DWuybbxkh4LXzMLGdMoBasK7Q&s',
+    function: () {},
+  ),CategoryModel(
+    name: 'PC and Lab',
+    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBGSEKSjd3QDc-fmnm6DWuybbxkh4LXzMLGdMoBasK7Q&s',
+    function: () {},
+  ),
+  CategoryModel(
+    name: 'Phones',
+    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBGSEKSjd3QDc-fmnm6DWuybbxkh4LXzMLGdMoBasK7Q&s',
+    function: () {},
+  ),CategoryModel(
+    name: 'PC and Lab',
+    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBGSEKSjd3QDc-fmnm6DWuybbxkh4LXzMLGdMoBasK7Q&s',
+    function: () {},
+  ),
+  CategoryModel(
+    name: 'Phones',
+    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBGSEKSjd3QDc-fmnm6DWuybbxkh4LXzMLGdMoBasK7Q&s',
+    function: () {},
+  ),CategoryModel(
+    name: 'PC and Lab',
+    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBGSEKSjd3QDc-fmnm6DWuybbxkh4LXzMLGdMoBasK7Q&s',
+    function: () {},
+  ),
+];
